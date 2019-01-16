dbde9cc4b6c9196f61d65771732ce6761471f419
Jota <josecarretero97@gmail.com>
2019-01-16 18:33:41 +0100

    Commit input film

--- peliculas/src/pages/home/home.ts
+++ peliculas/src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RestProvider } from '../../providers/rest/rest';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  movies: any;
+  constructor(public navCtrl: NavController, public restProvider: RestProvider) {
 
   }
 
+  getMovies() {
+
+
+  }
 }
+
+
--- peliculas/src/providers/rest/rest.ts
+++ peliculas/src/providers/rest/rest.ts
@@ -9,9 +9,23 @@ import { Injectable } from '@angular/core';
 */
 @Injectable()
 export class RestProvider {
+  num: String;
+  apiUrl = 'https://api.themoviedb.org/3/search/movie?api_key=6eb8f75b76dd7a332993ed9423c8a9d4&query=';
 
   constructor(public http: HttpClient) {
     console.log('Hello RestProvider Provider');
   }
+
+  getMovies() {
+    return new Promise(resolver => {
+      this.http.get(this.apiUrl + this.num).subscribe(data => {
+        resolver(data);
+      }, err => {
+        console.log(err);
+      });
+    });
+  }
+
+
 
 }
